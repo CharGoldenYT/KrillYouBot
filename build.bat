e9bd9b59b26a88b3e24dc1b99d09e47c8fd221a2
Anny (Char) <73309364+gameygu-0213@users.noreply.github.com>
2024-07-17 18:51:52 -0400

    hi

--- build.bat
+++ build.bat
@@ -1,1 +1,7 @@
+@echo off
+color 0A
+call pyinstaller\Scripts\activate.bat
 python pyinstaller\Lib\site-packages\PyInstaller\__main__.py -F --workpath ./export/build/ --distpath ./export/ krillyou.py
+copy botKey.txt export
+echo DONE
+pause
