d1f9fe0811d9918fadee4aa0cfefcdffc882521f
Anny (Char) <73309364+CharGoldenYT@users.noreply.github.com>
2024-10-14 13:30:46 -0400

    3.0h-1

--- src/modules/backend/startTasks.py
+++ src/modules/backend/startTasks.py
@@ -1,6 +1,8 @@
 from modules.commands.krillAbout import make_changelog
 from modules.backend.betterLogs.betterLogs import *
 from modules.commands.krillVersion import get_filname, compareVersions, getCurVersion
+from discord.guild import Guild
+from modules.backend.krillJson import initializeFTP
 
 allowReturns = False
 def check_allowReturns() -> bool:
@@ -20,7 +22,7 @@ def create_logsFolders():
         if str(e).endswith("file already exists: 'logs/'"):
             print('Logs folder already exists, doing nothing!')
         if not str(e).endswith("file already exists: 'logs/'"):
-            log_error(f'SHIT THERE WAS AN ERROR "{str(e)}"')
+            log_error(get_filname(), f'SHIT THERE WAS AN ERROR "{str(e)}"')
 
     try:
         os.makedirs('logs/old')
@@ -28,7 +30,36 @@ def create_logsFolders():
         if str(e).endswith("file already exists: 'logs/old'"):
             print('Old logs folder already exists, doing nothing!')
         if not str(e).endswith("file already exists: 'logs/old'"):
-            log_error(f'SHIT THERE WAS AN ERROR "{str(e)}"')
+            log_error(get_filname(), f'SHIT THERE WAS AN ERROR "{str(e)}"')
+
+    try:
+        os.makedirs('serverSettings/')
+    except OSError as e:
+        if str(e).endswith("file already exists: 'serverSettings/'"):
+            print('Server Settings folder already exists, doing nothing!')
+        if not str(e).endswith("file already exists: 'serverSettings/'"):
+            log_error(get_filname(), f'SHIT THERE WAS AN ERROR "{str(e)}"')
+
+
+def grab_serverSettings(servers:list[Guild]):
+    import os
+    for server in servers:
+        path = f'serverSettings/serverID-{str(server.id)}_Settings.json'
+        ftp = initializeFTP()
+        ftp.cwd('htdocs/krillYouBot_ServerSettings')
+        file = open(path, 'rb')
+        fileCompare = open(f'serverID-{str(server.id)}_Settings.temp', 'wb')
+        try:
+            ftp.retrbinary(f'RETR {path}', fileCompare.write); fileCompare.close()
+            fileCompare = open(f'serverID-{str(server.id)}_Settings.temp', 'rb')
+            if not fileCompare == file:
+                print('Files are different, replacing with upstream ver')
+                file = open(path, 'wb'); file.writelines(fileCompare.readlines()); file.close(); fileCompare.close()
+        except Exception as e:
+            fileCompare.close()
+            file.close()
+            log_err(get_filname(), f'There was an error trying to grab server settings for {str(server.id)}: "{str(e)}"')
+        os.remove(f'serverID-{str(server.id)}_Settings.temp')
 
 def run_startTasks():
     var = get_latestChangelog().lower()
