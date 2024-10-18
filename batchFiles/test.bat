c0d71fcf72f295ae209a11ce77b9fbfacf6d6c0c
Anny (Char) <73309364+CharGoldenYT@users.noreply.github.com>
2024-10-18 02:19:55 -0400

    Make seperate testing bat file

--- batchFiles/test.bat
+++ batchFiles/test.bat
@@ -2,5 +2,5 @@
 color 0A
 cd ..
 pyinstaller -F --workpath ./export/build/ --distpath ./export/ --icon=icon.ico --name "Krill You Bot" src/krillyou.py
-echo DONE
-pause
+cd export
+start ./"Krill You Bot.exe"
